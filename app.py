ccd13dc3cf981f9757c1a704869faa6526156247
Matthew Southworth <mdsouthworth1@gmail.com>
2023-02-22 02:47:13 -0500

    Removal of unneeded files

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import random
 import json
 import pickle
 from datetime import datetime
-from flask_ngrok import run_with_ngrok
+#from flask_ngrok import run_with_ngrok
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
@@ -89,12 +89,18 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
-        
+
 #define routes
 @app.route("/")
 @app.route("/home")
